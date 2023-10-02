1c1f051877e67ea4923c56b5e23740e2e12571ae
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2023-10-02 09:15:08 +0530

    knoir: Include hardware/google/interfaces and hardware/google/pixel
     * Required by power-libperfmgr
    
    Change-Id: I4053938614be26c7066ea83d97955739f39b4144
    Signed-off-by: HeroBuxx <me@herobuxx.me>
    Signed-off-by: Raya Mahardika <rayapkun@hlcyn.co>

--- knoir.mk
+++ knoir.mk
@@ -297,6 +297,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/motorola
 
 # Telephony
